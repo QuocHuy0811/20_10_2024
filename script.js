dd9a53f0367b74aa09a660dbaf61ab8b6926b58f
QHuy0811 <23110103@student.hcmute.edu.vn>
2024-10-20 15:14:06 +0700

    Upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function rain() {
+    let clound = document.querySelector('.clound');
+    let e = document.createElement('div');
+
+    e.classList.add('drop');
+    clound.appendChild(e);
+
+    let left = Math.floor(Math.random() * 290);
+    let size = Math.round() * 1.5;
+    let duration = Math.random();
+
+    e.innerText = '🩷';
+    e.style.left = left + 'px';
+    e.style.fontSize = 0.5 + size + 'em';
+    e.style.animationDirection = 1 + duration + 's';
+
+    setTimeout(function () {
+        clound.removeChild(e);
+    }, 2000)
+}
+
+setInterval(function () {
+    rain();
+}, 20)
